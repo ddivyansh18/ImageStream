3e359c5a6aaf47085e20462b11294a6872f7f09d
Divyansh Dixit <ddivyansh18@gmail.com>
2021-02-14 21:51:04 +0530

    Updated swagger url

--- src/components/about.js
+++ src/components/about.js
@@ -2,5 +2,5 @@ import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 
 export default function App(){ 
-    return <SwaggerUI url="https://api.jsonbin.it/bins/uXa1SRUH" tryItOutEnabled={false}/>
+    return <SwaggerUI url="https://api.jsonbin.it/bins/ugT5fWt4"/>
 }
